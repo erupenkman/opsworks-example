4ee43de819c09b858bac2d7bb33cd4094705dc31
eru <epenkman@thoughtworks.com>
2015-01-26 15:18:50 +1000

    now it works on vagrant

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'berks-fuck-you'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures berks-fuck-you'
+long_description 'Installs/Configures berks-fuck-you'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: berks-fuck-you
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file '/bin/motd' do
+  content 'sick this is working'
+end
