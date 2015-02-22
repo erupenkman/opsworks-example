4b4924f90fa4892e6d785ac615e628fa6836d024
eru <epenkman@thoughtworks.com>
2015-02-22 22:17:28 +1000

    script wrapper for aws

---
+++ bin/run-aws.sh
@@ -0,0 +1,22 @@
+stackId=3fb88cac-65ed-40f1-bde6-9d127e944e5c
+appId=1b4e3439-5616-480c-852a-2d5dbda1edab
+
+if [ "$1" = "install_dependencies" ]
+then
+   aws opsworks --region us-east-1 create-deployment --stack-id $stackId --command "{\"Name\":\"install_dependencies\"}"
+elif [ "$1" = "describe-deployments" ]
+then
+   aws opsworks --region us-east-1 describe-deployments --stack-id $stackId
+elif [ "$1" = "setup" ]
+then
+   aws opsworks --region us-east-1 create-deployment --stack-id $stackId --command "{\"Name\":\"setup\"}"
+elif [ "$1" = "deploy" ]
+then
+
+   aws opsworks --region us-east-1 create-deployment --app-id $appId --stack-id $stackId --command "{\"Name\":\"deploy\"}"
+
+
+else
+   echo "unknown command \"$1\" "
+fi
+
